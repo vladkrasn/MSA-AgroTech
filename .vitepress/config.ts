a7e07ab20f81e3c7b0c88ffebb389856c0d7de9c
Vladimir Krasnochub <vkrasnochub@saminfotech.com>
2025-07-19 00:17:47 +0400

    Vitepress comeback

---
+++ .vitepress/config.ts
@@ -0,0 +1,109 @@
+import { defineConfig } from "vitepress";
+import { configureDiagramsPlugin } from "vitepress-plugin-diagrams";
+
+export default defineConfig({
+  markdown: {
+    config: (md) => {
+      configureDiagramsPlugin(md, {
+        diagramsDir: ".vitepress/diagrams",
+        publicPath: ".vitepress/diagrams",
+      });
+    },
+  },
+  themeConfig: {
+		sidebar: [
+			{
+				text: "Tasks",
+				base: "/tasks",
+				items: [
+          {
+						text: "Task1",
+						base: "/Task1",
+            items: [
+              {
+                text: "Solution 1",
+                base: "/Task1/Solution1",
+                items: [
+                  { text: "ADR", link: "/adr" },
+                  { text: "C1 Diagram", link: "/c1" },
+                ]
+              },
+              {
+                text: "Solution 2",
+                base: "/Task1/Solution2",
+                items: [
+                  { text: "ADR", link: "/adr" },
+                  { text: "C1 Diagram", link: "/c1" },
+                ]
+              },
+            ]
+					},
+          {
+						text: "Task2",
+						base: "/Task2",
+            items: [
+              {
+                text: "Solution 1",
+                base: "/Task2/Solution1",
+                items: [
+                  { text: "ADR", link: "/adr" },
+                  { text: "C2 Diagram", link: "/c2" },
+                ]
+              },
+              {
+                text: "Solution 2",
+                base: "/Task2/Solution2",
+                items: [
+                  { text: "ADR", link: "/adr" },
+                  { text: "C2 Diagram", link: "/c2" },
+                ]
+              },
+            ]
+					},
+          {
+						text: "Task3",
+						base: "/Task3",
+            items: [
+              {
+                text: "Solution 1",
+                base: "/Task3/Solution1",
+                items: [
+                  { text: "C3 Diagram", link: "/c3" },
+                  { text: "C4 Diagram", link: "/c4" },
+                ]
+              },
+              {
+                text: "Solution 2",
+                base: "/Task3/Solution2",
+                items: [
+                  { text: "C3 Diagram", link: "/c3" },
+                  { text: "C4 Diagram", link: "/c4" },
+                ]
+              },
+            ]
+					},
+          {
+						text: "Task4",
+						base: "/Task4",
+            items: [
+              {
+                text: "Solution 1",
+                base: "/Task4/Solution1",
+                items: [
+                  { text: "ADR", link: "/adr" },
+                ]
+              },
+              {
+                text: "Solution 2",
+                base: "/Task4/Solution2",
+                items: [
+                  { text: "ADR", link: "/adr" },
+                ]
+              },
+            ]
+					},
+				],
+			},
+		],
+	},
+});
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Learning project
+
+## Installation
+
+```bash
+npm install
+```
+
+## Viewing
+
+```bash
+npm run docs:dev
+```
+
+## Diagram .SVGs
+
+They are located in `.vitepress/diagrams`
